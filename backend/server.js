e6b0876f87febfff19ce200eac8738e4eb57b21a
Yash Purohit <ashpurohit2001@gmail.com>
2021-05-21 00:21:44 +0530

    Edited some node server and installed mongodb integration

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,9 @@ const app = express()
 const port = 3000
 
 // Static Files
-app.use(express.static('C:/Y_Drive/WebDev/hotel-website/hotel-website'));
+app.use(express.static(__dirname+'/public'));
 // Specific folder example
-app.use('C:/Y_Drive/WebDev/hotel-website/hotel-website', express.static('C:/Y_Drive/WebDev/hotel-website/hotel-website'))
+app.use('C:/Y_Drive/WebDev/hotel-website/hotel-website', express.static(__dirname+'/public'))
 // app.use('/js', express.static(__dirname + 'public/js'))
 // app.use('/img', express.static(__dirname + 'public/images'))
 
@@ -19,29 +19,29 @@ app.get('', (req, res) => {
 })
 
 app.get('/', (req, res) => {
-    res.sendFile('C:/Y_Drive/WebDev/hotel-website/hotel-website/index.html')
+    res.sendFile(__dirname+'/public/index.html')
 })
 
 app.get('/contact', (req, res) => {
-    res.sendFile('C:/Y_Drive/WebDev/hotel-website/hotel-website/contact.html')
+    res.sendFile(__dirname+'/public/contact.html')
 })
 
 app.get('/menu', (req, res) => {
-    res.sendFile('C:/Y_Drive/WebDev/hotel-website/hotel-website/menu.html')
+    res.sendFile(__dirname+'/public/menu.html')
 })
 
 app.get('/dine_reservation', (req, res) => {
-    res.sendFile('C:/Y_Drive/WebDev/hotel-website/hotel-website/dinereservation.html')
+    res.sendFile(__dirname+'/public/dinereservation.html')
 })
 
 app.get('/room_reservation', (req, res) => {
-    res.sendFile('C:/Y_Drive/WebDev/hotel-website/hotel-website/room.html')
+    res.sendFile(__dirname+'/public/room.html')
 })
 
 app.get('/feedback', (req, res) => {
-    res.sendFile('C:/Y_Drive/WebDev/hotel-website/hotel-website/feedback.html')
+    res.sendFile(__dirname+'/public/feedback.html')
 })
 
 
 
-app.listen(port, () => console.info(`App listening on port ${port}`))
+app.listen(port, () => console.info(`App listening on port ${port} `+__dirname))

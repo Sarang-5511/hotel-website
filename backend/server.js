02885e30cbeb4bdb959c194efa630aae20b2572f
Yash Purohit <ashpurohit2001@gmail.com>
2021-05-21 10:38:17 +0530

    MongoDB for feedback data insertion added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,32 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const ejs = require('ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Set View's
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+mongoose.connect("mongodb://localhost:27017/hotelDB",{ useNewUrlParser: true ,  useUnifiedTopology: true });
+
+const feedbackSchema= {
+    f_name: String,
+    l_name: String,
+    email: String,
+    Address: String,
+    mobile: Number,
+    subject: String,
+    comment: String
+
+}
+
+const Feedback =mongoose.model("Feedback",feedbackSchema);
+
+
 
 // Static Files
 app.use(express.static(__dirname+'/public'));
@@ -9,12 +35,10 @@ app.use('C:/Y_Drive/WebDev/hotel-website/hotel-website', express.static(__dirnam
 // app.use('/js', express.static(__dirname + 'public/js'))
 // app.use('/img', express.static(__dirname + 'public/images'))
 
-// Set View's
-app.set('views', './views');
-app.set('view engine', 'ejs');
+
 
 // Navigation
-app.get('', (req, res) => {
+app.get('/ejs', (req, res) => {
     res.render('index', { text: 'Hey' })
 })
 
@@ -42,6 +66,18 @@ app.get('/feedback', (req, res) => {
     res.sendFile(__dirname+'/public/feedback.html')
 })
 
-
+app.post("/",function(req,res){
+    let newFeedback = new Feedback({
+        f_name: req.body.firstname,
+        l_name: req.body.lastname,
+        email: req.body.email,
+        Address: req.body.Address,
+        mobile: req.body.mobile,
+        subject: req.body.subject,
+        comment: req.body.comment        
+    });
+    newFeedback.save();
+    res.redirect('/');
+})
 
 app.listen(port, () => console.info(`App listening on port ${port} `+__dirname))
